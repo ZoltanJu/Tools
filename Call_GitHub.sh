0931d912d89c79968b8f2d104b4f5a38adcfefe2
Zoltan <71275392+ZoltanJu@users.noreply.github.com>
2021-11-09 09:43:12 +0000

    1st

---
+++ Call_GitHub.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo Token:
+read token
+if [ "$1"x != "x" ]; then
+        FileName=$1
+else
+        echo FileName:
+        read FileName
+fi
+Username="ZoltanJu"
+Repo="Scripts"
+
+curl -s -H "Authorization: token $token" -H 'Accept: application/vnd.github.v3.raw' -L "https://api.github.com/repos/$Username/$Repo/contents/$FileName" | sh
